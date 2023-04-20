6577401f0eb785d2395012f33770e0ae82f2b543
marina-alekseenko <alekseenko.ads@gmail.com>
2023-04-20 19:21:36 +0300

    sum of randoms

--- programm-sum/Program.cs
+++ programm-sum/Program.cs
@@ -1,4 +1,7 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+// выберет рандомное целое число от 1 до 9
 int result = numberA + numberB;
 Console.WriteLine(result);
